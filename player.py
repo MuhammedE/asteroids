509f3629aa177d96f0748afefb8c614bca1c817e
MuhammedE <muhammed.elma@gmail.com>
2025-02-21 20:21:06 +0100

    added moving player

--- player.py
+++ player.py
@@ -15,7 +15,11 @@ class Player(CircleShape):
         c = self.position - forward * self.radius + right
         return [a, b, c]
     def rotate(self,dt):
-        self.rotation += PLAYER_TURN_SPEED * dt    
+        self.rotation += PLAYER_TURN_SPEED * dt
+
+    def move(self,dt):
+        forward = pygame.Vector2(0,1).rotate(self.rotation)
+        self.position += forward * PLAYER_SPEED * dt    
 
     def draw(self,screen):
         white = pygame.Color(255,255,255)
@@ -28,3 +32,7 @@ class Player(CircleShape):
             self.rotate(-dt)
         if keys[pygame.K_d]:
             self.rotate(dt)
+        if keys[pygame.K_s]:
+            self.move(-dt)
+        if keys[pygame.K_w]:
+            self.move(dt)
